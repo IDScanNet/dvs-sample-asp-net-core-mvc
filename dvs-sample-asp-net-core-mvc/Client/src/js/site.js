d135a55335a7f23e2b30286097816588c046d770
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-04-01 17:04:11 -0500

    first commit

---
+++ dvs-sample-asp-net-core-mvc/Client/src/js/site.js
@@ -0,0 +1,96 @@
+﻿import IDVC from "@idscan/idvc";
+import "../../node_modules/@idscan/idvc/dist/css/idvc.css";
+
+
+let idvc = new IDVC({
+    el: "videoCapturingEl",
+    networkUrl: "/networks",
+    licenseKey: "",
+    types: ["ID", "Passport", "PassportCard"],
+    showSubmitBtn: true,
+    showForceCapturingBtn: false,
+    enableLimitation: false,
+    autoContinue: true,
+    isShowVersion: true,
+    resizeUploadedImage: 1500,
+    realFaceMode: "all",
+    minPDFframes: 4,
+    capturingMode: 4,
+    onCameraError(err) {
+        console.error(err);
+    },
+    submit(data) {
+        console.log('submit hanlder is running');
+        idvc.showSpinner(true);
+        let backStep = data.steps.find((item) => item.type === "back");
+        let trackString = null;
+        let backImage = null;
+
+        if (backStep) {
+            trackString = backStep && backStep.trackString ? backStep.trackString : "";
+            backImage = backStep.img.split(/:image\/(jpeg|png);base64,/)[2]
+        }
+
+
+        let request = {
+            frontImageBase64: data.steps
+                .find((item) => item.type === "front")
+                .img.split(/:image\/(jpeg|png);base64,/)[2],
+            backOrSecondImageBase64: backImage,
+            faceImageBase64: data.steps
+                .find((item) => item.type === "face")
+                .img.split(/:image\/(jpeg|png);base64,/)[2],
+            documentType: data.documentType,
+            trackString: trackString,
+            userAgent: window.navigator.userAgent,
+            captureMethod: data.captureMethod,
+            verifyFace: true,
+        };
+
+
+        fetch("https://dvs2.idware.net/api/v3/Request", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                Authorization: "Bearer ",
+            },
+            body: JSON.stringify(request),
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                fetch(
+                    "https://us-central1-idscan-backend.cloudfunctions.net/api/ValidationRequests/complete",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json;charset=utf-8",
+                        },
+                        body: JSON.stringify({
+                            requestId: response.requestId,
+                        }),
+                    }
+                )
+                    .then((response) => response.json())
+                    .then((response) => {
+                        document.getElementById("confidence").value =
+                            response.documentVerificationResult.totalConfidence;
+
+                        document.getElementById("faceMatch").value =
+                            response.faceVerificationResult.confidence;
+
+                        document.getElementById("antiSpoofing").value =
+                            response.faceVerificationResult.antiSpoofing;
+
+                        document.getElementById("far").value =
+                            response.faceVerificationResult.far;
+                        console.log(response);
+                        idvc.showSpinner(false);
+                    });
+            })
+            .catch((err) => {
+                idvc.showSpinner(false);
+                console.log("Error:", err);
+            });
+    }
+});
+
